45f4eb22eb690dc985e0f339b1be92cf0ffbe5b1
DingKay <pigsties@outlook.com>
2018-11-23 15:32:01 +0800

    Review Thread And Go Deep

---
+++ thread/src/dk/thread/test/printnums/PrintOddNum.java
@@ -0,0 +1,10 @@
+package dk.thread.test.printnums;
+
+/**
+ * @author DingKai
+ * @Classname PrintOddNum
+ * @Description TODO
+ * @create 2018-11-23
+ */
+public class PrintOddNum {
+}
---
+++ thread/src/dk/thread/test/printnums/TestToPrintNumbers.java
@@ -0,0 +1,10 @@
+package dk.thread.test.printnums;
+
+/**
+ * @author DingKai
+ * @Classname TestToPrintNumbers
+ * @Description TODO
+ * @create 2018-11-23
+ */
+public class TestToPrintNumbers {
+}
