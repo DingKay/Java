111d5e7ee72c65788745a728b34bc4a01bf0dd33
DingKay <pigsties@outlook.com>
2018-11-29 10:24:09 +0800

    Test Bean

---
+++ mybatis/src/main/java/com/dk/mybatis/dao/Test.java
@@ -0,0 +1,29 @@
+package com.dk.mybatis.dao;
+
+/**
+ * @author DingKai
+ * @Classname Test
+ * @Description TODO
+ * @create 2018-11-28
+ */
+public class Test {
+    public int id;
+
+    public String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
