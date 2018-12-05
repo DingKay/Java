edb21a9f6f4c018f566c1912de972f6e09c21c6c
DingKay <pigsties@outlook.com>
2018-12-05 14:38:49 +0800

    Modify

---
+++ mybatis/src/main/java/com/dk/mybatis/dao/TestDao.java
@@ -0,0 +1,11 @@
+package com.dk.mybatis.dao;
+
+/**
+ * @author DingKai
+ * @Classname TestDao
+ * @Description TODO
+ * @create 2018-12-05
+ */
+public interface TestDao {
+    int getTable(String tablename);
+}
