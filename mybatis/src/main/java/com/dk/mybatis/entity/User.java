215d3aba98c406604dbe8dea37de47cda273b59f
DingKay <pigsties@outlook.com>
2018-12-05 15:38:52 +0800

    Create User.java

---
+++ mybatis/src/main/java/com/dk/mybatis/entity/User.java
@@ -0,0 +1,29 @@
+package com.dk.mybatis.entity;
+
+/**
+ * @author DingKai
+ * @Classname User
+ * @Description TODO
+ * @create 2018-12-05
+ */
+public class User {
+    private int id;
+
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
