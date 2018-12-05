3c2677f915630eba5892b94bb96e14b007dfea1e
DingKay <pigsties@outlook.com>
2018-12-05 15:38:50 +0800

    Create UserLoginController.java

---
+++ mybatis/src/main/java/com/dk/mybatis/controller/UserLoginController.java
@@ -0,0 +1,10 @@
+package com.dk.mybatis.controller;
+
+/**
+ * @author DingKai
+ * @Classname UserLoginController
+ * @Description TODO
+ * @create 2018-12-05
+ */
+public class UserLoginController {
+}
