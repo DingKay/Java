1e7f2c5802d7a87b4d0175d19c19873ad7f14fa9
DingKay <pigsties@outlook.com>
2018-12-05 15:39:05 +0800

    Update ToTest.java

--- mybatis/src/test/java/com/dk/test/ToTest.java
+++ mybatis/src/test/java/com/dk/test/ToTest.java
@@ -1,5 +1,6 @@
 package com.dk.test;
 
+import com.dk.mybatis.entity.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -27,18 +28,15 @@ public class ToTest {
         //会话
         SqlSession session=sf.openSession();
         //01.弱类型  通过命名空间唯一锁定要执行的sql
-        // List<Department> list = session.selectList("cn.happy.dao.getAllDept");
+        // List<Department> list = session.selectList("cn.happy.mapper.getAllDept");
         //02.强类型
-//        List<com.dk.mybatis.dao.Test> list = session.selectList("getAllTest");
-//        for (com.dk.mybatis.dao.Test test : list) {
-//            System.out.println(test.getName());
-//        }
+        List<User> list = session.selectList("getAllTest");
+        for (User test : list) {
+            System.out.println(test.getName());
+        }
 //        session.update("createTable");
-//        com.dk.mybatis.dao.Test t = new com.dk.mybatis.dao.Test();
-//        t.setTablename("t_hi");
-//        int result = session.selectOne("getTable",t);
-        int result = session.selectOne("getTable","t_hi");
-        System.out.println("result = " + result);
+//        int result = session.selectOne("getTable","t_hi");
+//        System.out.println("result = " + result);
     }
 
 }
