1f35bfdd809d5df908d13f912ac635a90d07d75d
DingKay <pigsties@outlook.com>
2018-12-24 18:08:59 +0800

    Enumerate

---
+++ Java/src/com/dk/enumerate/Color.java
@@ -0,0 +1,12 @@
+package com.dk.enumerate;
+
+/**
+ * @author DingKai
+ * @Classname Color
+ * @Description TO Learn Enumerate
+ * @create 2018/12/24
+ */
+public enum Color {
+    GREEN,BLUE,RED,YELLOW
+}
+
---
+++ Java/src/com/dk/enumerate/Test.java
@@ -0,0 +1,14 @@
+package com.dk.enumerate;
+
+/**
+ * @author DingKai
+ * @Classname Test
+ * @Description TODO
+ * @create 2018/12/24
+ */
+public class Test {
+    public static void main(String[] args) {
+        //1.Enumerate Constant
+        System.out.println("red ==> " + Color.RED + "\n" + "blue ==> " + Color.BLUE);
+    }
+}
