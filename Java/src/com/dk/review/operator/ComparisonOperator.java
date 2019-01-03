25b8efa666cd7faa8ca31ace8a90be3ed6f0cbc6
DingKay <pigsties@outlook.com>
2019-01-03 16:32:17 +0800

    Modify

--- Java/src/com/dk/review/operator/ComparisonOperator.java
+++ Java/src/com/dk/review/operator/ComparisonOperator.java
@@ -4,7 +4,7 @@ public class ComparisonOperator {
     public static void main(String[] args) {
         int a = 10;
         int b = 10;
-        //基本数据类型相比较 == 判断的是值
+        //
         System.out.println(a == b);//true
 
         Thread t1 = new Thread();
