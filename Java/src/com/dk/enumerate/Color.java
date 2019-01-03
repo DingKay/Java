694cdd8105dd00ebf91a2dffcca715aed22b3966
DingKay <pigsties@outlook.com>
2019-01-03 17:25:50 +0800

    Mofify

--- Java/src/com/dk/enumerate/Color.java
+++ Java/src/com/dk/enumerate/Color.java
@@ -10,3 +10,10 @@ public enum Color {
     GREEN,BLUE,RED,YELLOW
 }
 
+class Test1{
+    public static void main(String[] args) {
+        //1.Enumerate Constant
+        System.out.println("red ==> " + Color.RED + "\n" + "blue ==> " + Color.BLUE);
+        System.out.println("green ==> " + Color.GREEN + "\n" + "yellow ==> " + Color.YELLOW);
+    }
+}
