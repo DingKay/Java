11a1117af6f2a8d82e678ace9b3116f344598e5a
DingKay <pigsties@outlook.com>
2019-01-22 19:04:15 +0800

    One Question and To Learn

---
+++ Java/src/com/dk/annotation/AnnotationListenerTest.java
@@ -0,0 +1,91 @@
+package com.dk.annotation;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.annotation.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * @author DingKai
+ * @Classname AnnotationListenerTest
+ * @Description                 **** This Java Program Was Wrong ****
+ *              The Problem is here : 81 lines ActionListener al = listenerClazz.newInstance();
+ *              Because this program is internal Class, Before newInstance() U need had out Class's
+ *              Instance.
+ *              temporarily unable to solve.
+ *              TODO how to solve
+ * @create 2019/1/22
+ */
+public class AnnotationListenerTest {
+    @Target(ElementType.FIELD)
+    @Documented
+    @Inherited
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface ActionListenerAnno {
+        Class<? extends ActionListener> listener();
+    }
+
+    class okListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Confirm button is clicked");
+            JOptionPane.showMessageDialog(null, "confirmed");
+        }
+    }
+
+    class cancel implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Cancel button is clicked");
+            JOptionPane.showMessageDialog(null, "cancelled");
+        }
+    }
+
+    public static class ListenerTest {
+        JFrame jFrame = new JFrame("Test");
+
+        @ActionListenerAnno(listener = okListener.class)
+        private JButton ok = new JButton("confirm");
+
+        @ActionListenerAnno(listener = cancel.class)
+        private JButton cancel = new JButton("cancel");
+
+        public void init() throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+            JPanel jPanel = new JPanel();
+            jPanel.add(ok);
+            jPanel.add(cancel);
+            jFrame.add(jPanel);
+            ButtonListener.process(this);
+            jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            jFrame.pack();
+            jFrame.setLocationRelativeTo(null);
+            jFrame.setVisible(true);
+        }
+
+        public static void main(String[] args) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+            new ListenerTest().init();
+        }
+
+        static class ButtonListener {
+            public static void process(Object obj) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+                Class clazz = obj.getClass();
+                Field[] declaredFields = clazz.getDeclaredFields();
+                for (Field field : declaredFields) {
+                    field.setAccessible(true);
+                    ActionListenerAnno annotation = field.getAnnotation(ActionListenerAnno.class);
+                    Object jButtonObj = field.get(obj);
+                    if (annotation !=null && jButtonObj instanceof AbstractButton){
+//                        Constructor constructor = clazz.getConstructor(AnnotationListenerTest.class);
+
+                        Class<? extends ActionListener> listener = annotation.listener();
+                        System.out.println("listener = " + listener);
+                        ActionListener actionListener = listener.newInstance();
+                        ((AbstractButton) jButtonObj).addActionListener(actionListener);
+                    }
+                }
+            }
+        }
+    }
+}
--- Java/src/com/dk/annotation/CustomAnnotationFive.java
+++ Java/src/com/dk/annotation/CustomAnnotationFive.java
@@ -1,9 +1,7 @@
 package com.dk.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
+import java.lang.reflect.Method;
 
 /**
  * @author DingKai
@@ -14,16 +12,36 @@ import java.lang.annotation.Target;
 public class CustomAnnotationFive {
     @Target(ElementType.METHOD)
     @Retention(RetentionPolicy.RUNTIME)
-    @interface Test{}
+    @interface Test {
+    }
 
-    class Junit{
-        public void test1(){}
-        public void test2(){}
-        public void test3(){}
-        public void test4(){}
+    class Junit {
+        @Test
+        public void test1() {
+        }
+
+        public void test2() {
+        }
+
+        public void test3() {
+        }
+
+        @Test
+        public void test4() {
+        }
     }
 
     public static void main(String[] args) {
+        int anInt = 0, annInt = 0;
+        Method[] methods = Junit.class.getDeclaredMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(Test.class)) {
+                anInt++;
+            } else {
+                annInt++;
+            }
+        }
+        System.out.println("@Test Method " + anInt + "\n" + "No This Annotation " + annInt);
 
     }
 }
