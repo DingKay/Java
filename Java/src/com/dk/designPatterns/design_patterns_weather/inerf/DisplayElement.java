a3b52442216925f15db298ea1400da8c17a9c42f
DingKay <pigsties@outlook.com>
2019-02-13 17:58:44 +0800

    WeatherData

---
+++ Java/src/com/dk/designPatterns/design_patterns_weather/inerf/DisplayElement.java
@@ -0,0 +1,11 @@
+package com.dk.designPatterns.design_patterns_weather.inerf;
+
+/**
+ * @author DingKai
+ * @Classname DisplayElement
+ * @Description TODO
+ * @create 2019/2/13
+ */
+public interface DisplayElement {
+    void display();
+}
---
+++ Java/src/com/dk/designPatterns/design_patterns_weather/inerf/Subject.java
@@ -0,0 +1,10 @@
+package com.dk.designPatterns.design_patterns_weather.inerf;
+
+/**
+ * @author DingKai
+ * @Classname Subject
+ * @Description TODO
+ * @create 2019/2/13
+ */
+public interface Subject {
+}
