c2d1f4b8d81e3249561acb53e3227e0025a0aeda
DingKay <pigsties@outlook.com>
2019-02-19 14:33:07 +0800

    Simple Code Progress Bar

--- Java/src/plugin/progressBar_code/CodeProgressBar.java
+++ Java/src/plugin/progressBar_code/CodeProgressBar.java
@@ -10,6 +10,8 @@ public class CodeProgressBar {
     private double barLength;
     private double percentage;
     private int endIndex = 50;
+    private String head = "\033[36;4m";
+    private String end = "\033[0m";
 
     public CodeProgressBar(double barLength){
         this.barLength = barLength;
@@ -21,21 +23,30 @@ public class CodeProgressBar {
 
     public void showBarByPoint(double currentPoint){
         this.percentage = currentPoint/this.barLength;
-        System.out.print("\r");
-        System.out.print(makeBarBypercentage(this.percentage));
+        System.out.print("\r" + this.endIndex * percentage + "\r" + makeBarBypercentage(this.percentage));
     }
 
     public String makeBarBypercentage(double percentage){
-        int nowIndex = (int)(this.endIndex * percentage);
+        /*Transformation:
+        * The int type is used here for the better execution loop below
+        * But this also caused a problem of 'loss of precision'
+        * In order to solve this problem, the solution of 'five into one one' is adopted.
+        * So use Math's round method*/
+        int nowIndex = (int) Math.round(this.endIndex * percentage);
         StringBuilder sb = new StringBuilder("[");
         for (int i = 0; i < this.endIndex; i++) {
+            if (i == 0 && nowIndex != 0)
+                sb.append(head);
             if (i < nowIndex){
-                sb.append("#");
-            }
-            else {
+                sb.append("=");
+            } else if(i == nowIndex){
+                sb.append(end + ">");
+            } else {
                 sb.append(" ");
             }
         }
+        if (nowIndex == this.endIndex)
+            sb.append(end);
         sb.append("]  ").append(String.format("%.2f", percentage * 100)).append("%");
 
         return String.valueOf(sb);
