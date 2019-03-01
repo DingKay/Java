c00775a31fdfe75be1c38929a372800dfcae70ee
DingKay <pigsties@outlook.com>
2019-03-01 16:50:15 +0800

    😊

--- Spring MVC/src/main/java/com/dk/mvc/controller/WelcomePage.java
+++ Spring MVC/src/main/java/com/dk/mvc/controller/WelcomePage.java
@@ -3,6 +3,7 @@ package com.dk.mvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author DingKai
@@ -12,9 +13,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class WelcomePage {
+    @ResponseBody
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String Welcome(){
-
+        System.out.println("action hello");
         return "Welcome.";
     }
 }
