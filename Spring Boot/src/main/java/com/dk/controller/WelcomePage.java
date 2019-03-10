1fde7b1947025d79077e49644cfe6a1b5b5e65cf
DingKay <Pigsties@outlook.com>
2019-03-11 01:21:52 +0800

    modify path and name

---
+++ Spring Boot/src/main/java/com/dk/controller/WelcomePage.java
@@ -0,0 +1,18 @@
+package com.dk.controller;
+
+/**
+ * @author DingKai
+ * @Classname WelcomePage
+ * @Description TODO
+ * @create 2019/3/8
+ */
+//@RestController
+//public class WelcomePage {
+////    private static final Logger logger = LoggerFactory.getLogger(WelcomePage.class);
+//
+//    @RequestMapping("/index")
+//    public String welcome(){
+////        logger.info("Welcome Spring Boot!");
+//        return "index";
+//    }
+//}
