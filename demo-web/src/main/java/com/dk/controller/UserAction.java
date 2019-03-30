caceb1b6214b0fc673dc32e3c7a52a65e53b361d
DingKay <pigsties@outlook.com>
2019-03-30 16:11:32 +0800

    this pom modify is because 'javax.servlet-api' jar conflict, and scope element add 'provided'

--- demo-web/src/main/java/com/dk/controller/UserAction.java
+++ demo-web/src/main/java/com/dk/controller/UserAction.java
@@ -2,8 +2,10 @@ package com.dk.controller;
 
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.dk.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/user")
@@ -14,11 +16,13 @@ public class UserAction {
      * Remotely injection ,this annotation equals @AutoWired
      * */
     @Reference
-    private UserAction userAction;
+    private UserService userService;
 
     @RequestMapping("/get")
+    @ResponseBody
     public String getName(){
-        String name = userAction.getName();
+        System.out.println("UserAction.getName");
+        String name = userService.getName();
         return name;
     }
 }
