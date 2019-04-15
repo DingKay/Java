28810448fe272c24385ca220d8aaa31d9d0d7319
DingKay <pigsties@outlook.com>
2019-04-15 20:54:41 +0800

    Java project

--- Java/src/com/dk/review/operator/equal/ComparisonOperator.java
+++ Java/src/com/dk/review/operator/equal/ComparisonOperator.java
@@ -24,14 +24,31 @@ public class ComparisonOperator {
 
         /**
          * this '==' in Integer between -128 to 127 then true
-         * if not this interval then false
+         * if not this interval then false;
+         * the following is wrong example
          */
-        Integer common = 127;
+        /*Integer common = 127;
         Integer commoz = 127;
         System.out.println(common == commoz);
 
         Integer special = 128;
         Integer speciaz = 128;
-        System.out.println(special == speciaz);
+        System.out.println(special == speciaz);*/
+
+        Integer common = 127;
+        Integer commoz = 127;
+        System.out.println(common.equals(commoz));
+
+        Integer special = 128;
+        Integer speciaz = 128;
+        System.out.println(special.equals(speciaz));
+
+        /**
+         * note this order;
+         * if this example equals 'null',then will throws NullPointerException
+         */
+        Integer example = null;
+        System.out.println(example.equals(1));
+        System.out.println("1".equals(example));
     }
 }
