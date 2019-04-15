dfefd15d7f43cb4f4a331c527ddebaef4b43dcbf
DingKay <pigsties@outlook.com>
2019-04-15 17:36:17 +0800

    Java project 😊

---
+++ Java/src/com/dk/variable/VariableParameterType.java
@@ -0,0 +1,26 @@
+package com.dk.variable;
+
+/**
+ * @author DingKai
+ * @Classname VariableParameterType
+ * @Description To learn Method's Variable Parameter
+ * @create 2019/4/15
+ */
+public class VariableParameterType {
+
+    public static void main(String[] args) {
+        Integer dk = new VariableParameterType().getUserIds("dingkai");
+        System.out.println(dk);
+    }
+
+    /**
+     * Variable Parameter
+     */
+     private Integer getUserIds(String type, int... ids){
+        System.out.println("VariableParameterType.getUserIds");
+        System.out.println("Type = " + type);
+        // this variable ids toString method is Object's toString, so print result is variable address
+        System.out.println("ids = " + ids);
+        return 1;
+    }
+}
