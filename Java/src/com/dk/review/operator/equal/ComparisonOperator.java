3ecfac13b062dbbb4ee5209342ff04e3229e3447
DingKay <pigsties@outlook.com>
2019-04-15 20:55:56 +0800

    Java project

--- Java/src/com/dk/review/operator/equal/ComparisonOperator.java
+++ Java/src/com/dk/review/operator/equal/ComparisonOperator.java
@@ -10,19 +10,15 @@ public class ComparisonOperator {
     public static void main(String[] args) {
         int a = 10;
         int b = 10;
-        /**
-         * true
-         */
+        // true
         System.out.println(a == b);
 
         Thread t1 = new Thread();
         Thread t2 = new Thread();
-        /**
-         * false
-         */
+        // false
         System.out.println(t1 == t2);
 
-        /**
+        /*
          * this '==' in Integer between -128 to 127 then true
          * if not this interval then false;
          * the following is wrong example
@@ -43,12 +39,12 @@ public class ComparisonOperator {
         Integer speciaz = 128;
         System.out.println(special.equals(speciaz));
 
-        /**
+        /*
          * note this order;
          * if this example equals 'null',then will throws NullPointerException
          */
         Integer example = null;
-        /**
+        /*
          * the following is wrong example
          */
         System.out.println(example.equals(1));
