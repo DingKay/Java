0fd86865eb6eec81d6d6ab19469f4effc87c5067
DingKay <pigsties@outlook.com>
2019-04-25 09:59:43 +0800

    Java project

--- demo-web/src/main/java/com/dk/controller/UserAction.java
+++ demo-web/src/main/java/com/dk/controller/UserAction.java
@@ -3,6 +3,8 @@ package com.dk.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.dk.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,6 +12,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/user")
 public class UserAction {
+    /**
+     * slf4j日志对象
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(UserAction.class.getName());
 
     /**
      * @Description
