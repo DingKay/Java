41c28fc45adeb474020f3014584039815c37c478
DingKay <Pigsties@outlook.com>
2019-12-09 23:27:30 +0800

    Test

---
+++ Java/src/com/dk/geektime/everyday/BoxString.java
@@ -0,0 +1,18 @@
+package com.dk.geektime.everyday;
+
+/**
+ * @author DingKai
+ * @version 1.0
+ * @date 2019/12/9
+ */
+public class BoxString {
+    private String obj;
+
+    public String getObj() {
+        return obj;
+    }
+
+    public void setObj(String obj) {
+        this.obj = obj;
+    }
+}
---
+++ Java/src/com/dk/geektime/everyday/BoxT.java
@@ -0,0 +1,18 @@
+package com.dk.geektime.everyday;
+
+/**
+ * @author DingKai
+ * @version 1.0
+ * @date 2019/12/9
+ */
+public class BoxT<T> {
+    private T t;
+
+    public T getT() {
+        return t;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
+}
