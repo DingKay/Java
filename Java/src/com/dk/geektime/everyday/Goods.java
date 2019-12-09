2c22282687f62b10502660726666b92faa6d8326
DingKay <dingkai@vrvmail.com.cn>
2019-12-09 15:56:58 +0800

    使用反射优化代码

---
+++ Java/src/com/dk/geektime/everyday/Goods.java
@@ -0,0 +1,35 @@
+package com.dk.geektime.everyday;
+
+/**
+ * @author DingKai
+ * @version 1.0
+ * @date 2019/12/9
+ */
+public class Goods {
+    private String goodName = "good-name";
+    private String test = "test-abc";
+
+    public String getGoodName() {
+        return goodName;
+    }
+
+    public void setGoodName(String goodName) {
+        this.goodName = goodName;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    @Override
+    public String toString() {
+        return "Goods{" +
+                "goodName='" + goodName + '\'' +
+                ", test='" + test + '\'' +
+                '}';
+    }
+}
---
+++ Java/src/com/dk/geektime/everyday/TestMain.java
@@ -0,0 +1,50 @@
+package com.dk.geektime.everyday;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author DingKai
+ * @version 1.0
+ * @date 2019/12/9
+ */
+public class TestMain {
+    public static void main(String[] args) throws Exception {
+        System.out.println(ordToDoc(new Order()));
+
+        System.out.println(goodsToDoc(new Goods()));
+
+        System.out.println("====");
+
+        System.out.println(ordToDocNew(new Order()));
+
+        System.out.println(ordToDocNew(new Goods()));
+    }
+
+    public static Map<String, Object> ordToDoc(Order order) {
+        Map<String, Object> resultMap = new HashMap<>(3);
+        resultMap.put("goods",order.getGoods());
+        resultMap.put("price",order.getPrice());
+        resultMap.put("count",order.getCount());
+        return resultMap;
+    }
+
+    public static Map<String, Object> goodsToDoc(Goods goods) {
+        Map<String, Object> resultMap = new HashMap<>(2);
+        resultMap.put("goodName",goods.getGoodName());
+        resultMap.put("test",goods.getTest());
+        return resultMap;
+    }
+
+    public static HashMap<String, Object> ordToDocNew(Object obj) throws Exception {
+        HashMap<String, Object> resultMap = new HashMap<>(3);
+        Field[] declaredFields = obj.getClass().getDeclaredFields();
+
+        for (Field field : declaredFields) {
+            field.setAccessible(true);
+            resultMap.put(field.getName(), field.get(obj));
+        }
+        return resultMap;
+    }
+}
